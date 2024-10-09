480ef00022889a3e1a7c78ffc9fb9d8e9cec9446
giseleveloso <giseleveloso00@gmail.com>
2024-10-09 09:57:53 -0300

    CRUD Telefone

--- tenis-angular/src/app/app.routes.ts
+++ tenis-angular/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { EnderecoListComponent } from './components/endereco/endereco-list/endereco-list.component';
 import { EnderecoFormComponent } from './components/endereco/endereco-form/endereco-form.component';
 import { enderecoResolver } from './components/endereco/resolver';
+import { TelefoneListComponent } from './components/telefone/telefone-list/telefone-list.component';
+import { TelefoneFormComponent } from './components/telefone/telefone-form/telefone-form.component';
+import { telefoneResolver } from './components/telefone/resolver';
 
 export const routes: Routes = [
     {path: 'enderecos', component: EnderecoListComponent, title: 'Lista de Endereços'},
     {path: 'enderecos/new',component: EnderecoFormComponent, title: 'Novo Endereço'},
-    {path: 'enderecos/edit/:id',component: EnderecoFormComponent, resolve: {endereco: enderecoResolver}}
+    {path: 'enderecos/edit/:id',component: EnderecoFormComponent, resolve: {endereco: enderecoResolver}},
+
+    {path: 'telefones', component: TelefoneListComponent, title: 'Lista de Telefones'},
+    {path: 'telefones/new',component: TelefoneFormComponent, title: 'Novo Telefone'},
+    {path: 'telefones/edit/:id',component: TelefoneFormComponent, resolve: {telefone: telefoneResolver}}
 ];
