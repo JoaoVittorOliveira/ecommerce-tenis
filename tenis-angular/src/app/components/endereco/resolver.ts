2fc6b6fb069f8736b879089378ac7eab9cbea447
joao vittor oliveira <joaovittor324@gmail.com>
2024-10-09 13:27:07 -0300

    testando commit na branch

---
+++ tenis-angular/src/app/components/endereco/resolver.ts
@@ -0,0 +1,8 @@
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
+import { inject } from "@angular/core";
+import { EnderecoService } from "../../services/endereco.service";
+import { Endereco } from "../../models/endereco.model";
+export const enderecoResolver: ResolveFn<Endereco> =
+    (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+        return inject(EnderecoService).findById(route.paramMap.get('id')!);
+    }
---
+++ tenis-angular/src/app/services/telefone.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Telefone } from '../models/telefone.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TelefoneService {
+  private baseUrl = 'http://localhost:8080/telefones';
+
+  constructor(private httpClient : HttpClient){
+
+  }
+
+  findAll(): Observable<Telefone[]> {
+    return this.httpClient.get<Telefone[]>(this.baseUrl); 
+  }
+
+  findById(id: string): Observable<Telefone> {
+    return this.httpClient.get<Telefone>(`${this.baseUrl}/search/id/${id}`); 
+  }
+
+  insert(telefone: Telefone): Observable<Telefone> {
+    return this.httpClient.post<Telefone>(this.baseUrl, telefone);
+  }
+
+  update(telefone: Telefone): Observable<Telefone> {
+    const data = {
+      ddd: telefone.ddd,
+      numero: telefone.numero,
+    }
+    return this.httpClient.put<any>(`${this.baseUrl}/${telefone.id}`, data); 
+  }
+
+  delete(telefone: Telefone): Observable<any>{
+    return this.httpClient.delete<any>(`${this.baseUrl}/${telefone.id}`); 
+  }
+}
