840070fb2bc9c7d66af4dfbb04ab11613afc7cb5
giseleveloso <giseleveloso00@gmail.com>
2024-10-09 10:02:28 -0300

    CRUD telefone completo

--- tenis-quarkus/src/main/java/br/unitins/joaovittor/basqueteiros/Telefone/dto/TelefoneResponseDTO.java
+++ tenis-quarkus/src/main/java/br/unitins/joaovittor/basqueteiros/Telefone/dto/TelefoneResponseDTO.java
@@ -3,12 +3,14 @@ package br.unitins.joaovittor.basqueteiros.Telefone.dto;
 import br.unitins.joaovittor.basqueteiros.Telefone.model.Telefone;
 
 public record TelefoneResponseDTO (
+    Long id,
     String ddd,
     String numero
 ){
     
     public static TelefoneResponseDTO valueof(Telefone telefone){
         return new TelefoneResponseDTO(
+            telefone.getId(),
             telefone.getDdd(),
             telefone.getNumero()
         );
